f70dbbe6b6f3ff18e66a71da86bdf651f641dd95
Nguyen Van Truong <nguyenthanchua1202@gmail.com>
2026-05-17 22:11:58 +0700

    Sd

--- src/components/ExhibitionHeader.tsx
+++ src/components/ExhibitionHeader.tsx
@@ -2,27 +2,32 @@ import Image from "next/image";
 
 export default function ExhibitionHeader() {
   return (
-    <header className="exhibition-header relative z-10 w-full shrink-0">
-      <Image
-        src="/images/emblem-annd.png"
-        alt="Huy hiệu Công an nhân dân"
-        width={128}
-        height={128}
-        className="exhibition-emblem object-contain"
-        priority
-      />
-      <Image
-        src="/images/flag-vn.png"
-        alt="Cờ Tổ quốc"
-        width={150}
-        height={150}
-        className="exhibition-flag object-contain"
-        priority
-      />
+    <header className="exhibition-header">
+      <div className="exhibition-header-side exhibition-header-side--left">
+        <Image
+          src="/images/emblem-annd.png"
+          alt="Huy hiệu Công an nhân dân"
+          width={120}
+          height={120}
+          className="exhibition-emblem"
+          priority
+        />
+      </div>
 
       <div className="exhibition-header-titles">
         <p className="exhibition-title-sub">VAI TRÒ, UY TÍN</p>
         <h1 className="exhibition-title-main">LÃNH ĐẠO AN NINH NHÂN DÂN</h1>
+      </div>
+
+      <div className="exhibition-header-side exhibition-header-side--right">
+        <Image
+          src="/images/flag-vn.png"
+          alt="Cờ Tổ quốc"
+          width={168}
+          height={168}
+          className="exhibition-flag"
+          priority
+        />
       </div>
     </header>
   );
