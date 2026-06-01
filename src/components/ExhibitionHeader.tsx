87b735d12e85bcf7ee56889f7d9096ed610314cd
Nguyen Van Truong <nguyenthanchua1202@gmail.com>
2026-06-01 18:00:59 +0700

    Sd

--- src/components/ExhibitionHeader.tsx
+++ src/components/ExhibitionHeader.tsx
@@ -16,14 +16,9 @@ export default function ExhibitionHeader() {
 
       <header className="exhibition-header">
         <div className="exhibition-header-titles">
-          <Image
-            src="/images/home-title.png"
-            alt="Vai trò, uy tín - Lãnh đạo An ninh nhân dân"
-            width={1024}
-            height={128}
-            className="exhibition-title-image"
-            priority
-          />
+          <h1 className="exhibition-title-text">
+            TỪ LỰC LƯỢNG AN NINH NHÂN DÂN ĐẾN NHỮNG CƯƠNG VỊ LÃNH ĐẠO CHỦ CHỐT
+          </h1>
         </div>
       </header>
     </>
