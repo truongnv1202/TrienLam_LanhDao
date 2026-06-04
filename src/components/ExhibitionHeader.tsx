3b9fa69a48c8f1bd1bb29aee7f97b19e15514a0b
Your Name <you@example.com>
2026-06-04 16:45:33 +0700

    DS

--- src/components/ExhibitionHeader.tsx
+++ src/components/ExhibitionHeader.tsx
@@ -19,7 +19,7 @@ export default function ExhibitionHeader() {
         <div className="exhibition-header-titles">
           <h1 className="exhibition-title-text">
             <Image
-              src="/images/exhibition-title-text.png"
+              src="/images/exhibition-title-text.png?v=20260604-title-transparent"
               alt="Từ lãnh đạo cục thuộc lực lượng An ninh nhân dân đến những cương vị lãnh đạo chủ chốt"
               width={2597}
               height={248}
